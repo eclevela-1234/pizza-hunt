627df2f2a9120133c5e70bfcc1cebe4a45078070
Eliot Cleveland <eclevela@richmond.edu>
2022-09-24 20:59:32 -0400

    Added comment model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 // create the Pizza model using the PizzaSchema
 const Pizza = require('./Pizza');
+const Comment = require('./Comment')
 
 
 // export the Pizza model
-module.exports = { Pizza }
+module.exports = { Pizza, Comment}
 
